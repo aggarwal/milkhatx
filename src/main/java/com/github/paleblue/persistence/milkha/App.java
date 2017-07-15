0c0f1cfa26f23a7d4cf16825eb8a652d4cf9de7e
Shitanshu Aggarwal <saggarwal.90@gmail.com>
2017-07-15 13:40:15 -0700

    bootstrapping maven repository

---
+++ src/main/java/com/github/paleblue/persistence/milkha/App.java
@@ -0,0 +1,13 @@
+package com.github.paleblue.persistence.milkha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
