7842aabf6bf56b8646c9a11c121cd8ff6157a7c4
Shitanshu Aggarwal <saggarwal.90@gmail.com>
2017-07-15 17:20:33 -0700

    building correct sqlite path for ddb local

--- src/test/java/com/github/paleblue/persistence/milkha/DynamoDBLocalAbstractTest.java
+++ src/test/java/com/github/paleblue/persistence/milkha/DynamoDBLocalAbstractTest.java
@@ -1,20 +1,19 @@
 package com.github.paleblue.persistence.milkha;
 
-
-import java.io.File;
-import java.util.UUID;
-
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.local.embedded.DynamoDBEmbedded;
-import com.github.paleblue.persistence.milkha.mapper.HashOnlyMapper;
-import com.github.paleblue.persistence.milkha.mapper.TransactionLogItemMapper;
 import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.github.paleblue.persistence.milkha.mapper.HashOnlyMapper;
+import com.github.paleblue.persistence.milkha.mapper.TransactionLogItemMapper;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
 
 @Ignore
 public abstract class DynamoDBLocalAbstractTest {
@@ -22,9 +21,28 @@ public abstract class DynamoDBLocalAbstractTest {
     protected final AmazonDynamoDB ddbClient;
 
     protected DynamoDBLocalAbstractTest() {
-        System.out.println("PROPERTY" + System.getProperty("sqlite4java.library.path"));
-        // System.setProperty("sqlite4java.library.path", System.getProperty("user.dir")+"/lib/sqlite4java-392/");
-        ddbClient = DynamoDBEmbedded.create(new File("/Users/shitanshu/ddblocal.sqlite")).amazonDynamoDB();
+        ddbClient = DynamoDBEmbedded.create().amazonDynamoDB();
+    }
+
+    @BeforeClass
+    public static void setSqlitePath() {
+        final String sqliteJarPath = System.getProperty("sqlite4java.jar.path");
+        if (sqliteJarPath == null) {
+            return;
+        }
+        final Path sqliteJar = Paths.get(sqliteJarPath);
+        final String sqliteNativePath;
+        switch (System.getProperty("os.name")) {
+            case "Mac OS X":
+                int length = sqliteJar.getNameCount();
+                final String version = sqliteJar.getName(length-2).toString();
+                final String groupPath = sqliteJar.getParent().getParent().getParent().toString();
+                sqliteNativePath = Paths.get(groupPath, "libsqlite4java-osx", version).toString();
+                break;
+            default:
+                sqliteNativePath = sqliteJar.toString();
+        }
+        System.setProperty("sqlite4java.library.path", sqliteNativePath);
     }
 
     @Before
